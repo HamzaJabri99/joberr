8a8cc2227bc5361e6517b51f4833da9264b8b87c
unknown <jabri.oldd@gmail.com>
2023-02-24 03:11:23 +0100

    changing navbar on scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { React } from "react";
 import Navbar from "./components/navbar/Navbar";
 function App() {
-  return <div className="App"><Navbar/></div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Navbar.scss";
 const Navbar = () => {
+  const [active, setActive] = useState(false);
+  const scrololo = () => {
+    window.scrollY > 20 ? setActive(true) : setActive(false);
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", scrololo);
+    return () => {
+      window.removeEventListener("scroll", scrololo);
+    };
+  }, []);
   return (
-    <div className="navbar">
+    <div className={active ? "navbar active" : "navbar"}>
       <div className="container">
         <div className="logo">
-        
-          {/*<Link to="/">*/}<span className="text">joberr</span>{/*</Link>*/}
+          {/*<Link to="/">*/}
+          <span className="text">joberr</span>
+          {/*</Link>*/}
           <span className="dot">.</span>
         </div>
         <div className="links">
-          <span>Joberr Business</span> 
+          <span>Joberr Business</span>
           <span>Explore</span>
           <span>English</span>
           <span>Become a Seller</span>
@@ -18,18 +29,22 @@ const Navbar = () => {
           <button>Join</button>
         </div>
       </div>
-      <hr />
-      <div className="menu">
-      <span>Test</span>
-      <span>Test2</span>
-      <span>Test3</span>
-      <span>Test4</span>
-      <span>Test5</span>
-      <span>Test6</span>
-      <span>Test7</span>
-      <span>Test8</span>
-      <span>Test9</span>
-      </div>
+      {active && (
+        <>
+          <hr />
+          <div className="menu">
+            <span>Test</span>
+            <span>Test2</span>
+            <span>Test3</span>
+            <span>Test4</span>
+            <span>Test5</span>
+            <span>Test6</span>
+            <span>Test7</span>
+            <span>Test8</span>
+            <span>Test9</span>
+          </div>
+        </>
+      )}
     </div>
   );
 };
