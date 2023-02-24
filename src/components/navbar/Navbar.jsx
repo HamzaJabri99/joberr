308705b3860c0254966bb3c00351f27839b8e51e
unknown <jabri.oldd@gmail.com>
2023-02-24 02:00:24 +0100

    adding menu section

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,17 +5,30 @@ const Navbar = () => {
     <div className="navbar">
       <div className="container">
         <div className="logo">
-          <span className="text">joberr</span>
+        
+          {/*<Link to="/">*/}<span className="text">joberr</span>{/*</Link>*/}
           <span className="dot">.</span>
         </div>
         <div className="links">
-          <span>Joberr Business</span>
+          <span>Joberr Business</span> 
           <span>Explore</span>
           <span>English</span>
           <span>Become a Seller</span>
           <span>Sign in</span>
           <button>Join</button>
         </div>
+      </div>
+      <hr />
+      <div className="menu">
+      <span>Test</span>
+      <span>Test2</span>
+      <span>Test3</span>
+      <span>Test4</span>
+      <span>Test5</span>
+      <span>Test6</span>
+      <span>Test7</span>
+      <span>Test8</span>
+      <span>Test9</span>
       </div>
     </div>
   );
