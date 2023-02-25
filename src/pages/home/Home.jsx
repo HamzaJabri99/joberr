dc4b165685f07073eee62e5abde0656093e90e09
unknown <jabri.oldd@gmail.com>
2023-02-25 18:44:11 +0100

    categories slider component using inifnit scroll lib

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import Featured from '../../components/featured/Featured'
-import TrustedBy from '../../components/trustedBy/TrustedBy'
+import React from "react";
+import Featured from "../../components/featured/Featured";
+import TrustedBy from "../../components/trustedBy/TrustedBy";
+import Slide from "../../components/slider/Slide";
+import CatCard from "../../components/catCard/CatCard";
+import { cards } from "../../data";
 const Home = () => {
   return (
-    <div><Featured/><TrustedBy/></div>
-  )
-}
+    <div>
+      <Featured />
+      <TrustedBy />
+      <Slide arrowsScroll={5} slidesToShow={5}>
+      {cards.map((card)=>{
+            return(<CatCard key={card.id} item={card}/>)
+            })}
+      </Slide>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
