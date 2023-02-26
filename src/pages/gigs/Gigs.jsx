befef823c5cc03f407da77874417a0193b4c6b27
unknown <jabri.oldd@gmail.com>
2023-02-26 23:59:03 +0100

    starting to design category page

--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -8,10 +8,25 @@ const Gigs = () => {
           JOBERR {">"} GRAPHICS & DESIGN {">"}
         </span>
         <h1>AI Artists</h1>
-        <p>Explore the boundries of art and technology with Joberr's AI artists</p>
+        <p>
+          Explore the boundries of art and technology with Joberr's AI artists
+        </p>
         <div className="menu">
-          <div className="left"></div>
-          <div className="right"></div>
+          <div className="left">
+            <span>Budget</span>
+            <input type="text" placeholder="min" />
+            <input type="text" placeholder="max" />
+            <button>Apply</button>
+          </div>
+          <div className="right">
+            <span className="sortBy">SortBy</span>
+            <span className="sortType">Best Selling</span>
+            <img src="./imgs/down.png" alt="" />
+            <div className="rightMenu">
+              <span>Newest</span>
+              <span>Best Selling</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
