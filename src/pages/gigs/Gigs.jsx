8a9942ffb689c1cffa94100e6d7cdd367c99afdc
unknown <jabri.oldd@gmail.com>
2023-02-26 23:49:37 +0100

    designing GIG card component

--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-
+import React from "react";
+import "./Gigs.scss";
 const Gigs = () => {
   return (
-    <div>Gigs</div>
-  )
-}
+    <div className="gigs">
+      <div className="container">
+        <span className="breadcrumbs">
+          JOBERR {">"} GRAPHICS & DESIGN {">"}
+        </span>
+        <h1>AI Artists</h1>
+        <p>Explore the boundries of art and technology with Joberr's AI artists</p>
+        <div className="menu">
+          <div className="left"></div>
+          <div className="right"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Gigs
+export default Gigs;
