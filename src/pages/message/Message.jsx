500b4e67f02005164c69912dfb4b86e37440d40a
unknown <jabri.oldd@gmail.com>
2023-02-28 21:26:53 +0100

    chat page design

--- src/pages/message/Message.jsx
+++ src/pages/message/Message.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
-
+import React from "react";
+import "./Message.scss";
+import { Link } from "react-router-dom";
 const Message = () => {
   return (
-    <div>Message</div>
-  )
-}
+    <div className="message">
+      <div className="container">
+        <span className="breadCrumbs">
+          <Link to={"/messages"} className="link">MESSAGES {">"} Jotaro Kujo</Link>
+        </span>
+        <div className="messages">
+          <div className="item">
+            <img
+              src="https://avatars.githubusercontent.com/u/75950318?v=4"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt
+              eveniet provident quibusdam velit ab dolorum nostrum quis
+              temporibus, ratione nesciunt aut aperiam maiores repudiandae eos
+              architecto a ullam facere! Consequuntur.
+            </p>
+          </div>
+          <div className="item owner">
+            <img
+              src="https://avatars.githubusercontent.com/u/75950318?v=4"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt
+              eveniet provident quibusdam velit ab dolorum nostrum quis
+              temporibus, ratione nesciunt aut aperiam maiores repudiandae eos
+              architecto a ullam facere! Consequuntur.
+            </p>
+          </div>
+          <div className="item">
+            <img
+              src="https://avatars.githubusercontent.com/u/75950318?v=4"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt
+              eveniet provident quibusdam velit ab dolorum nostrum quis
+              temporibus, ratione nesciunt aut aperiam maiores repudiandae eos
+              architecto a ullam facere! Consequuntur.
+            </p>
+          </div>
+          
+        </div>
+        <hr />
+        <div className="write">
+          <textarea
+            name=""
+            placeholder="write a message"
+            id=""
+            cols="30"
+            rows="10"
+          ></textarea>
+          <button>Send</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Message
+export default Message;
