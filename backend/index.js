d178ba64ee52751e16fa0de7136f4087e632996c
unknown <jabri.oldd@gmail.com>
2023-03-06 19:53:03 +0100

    connecting to mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 const app = express();
+dotenv.config();
+mongoose.set("strictQuery", true);
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("connected!");
+  } catch (err) {
+    console.log(err);
+  }
+};
 app.listen(8800, () => {
+  connect();
   console.log("v");
 });
