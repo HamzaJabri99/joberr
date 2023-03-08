791be88e653869ceba70a1b91dd1a5219ae6eea5
unknown <jabri.oldd@gmail.com>
2023-03-08 23:57:59 +0100

    creating login design and connecting to backend server using axios

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import "./Login.scss";
+import axios from "axios";
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:8800/api/auth/login", {
+        username,
+        password,
+      });
+      console.log(res);
+    } catch (e) {
+      setError(e);
+      console.log(e);
+    }
+  };
   return (
-    <div>Login</div>
-  )
-}
+    <div className="login">
+      <form onSubmit={handleSubmit}>
+        <h1>Sign in</h1>
+        <label>Username</label>
+        <input
+          type="text"
+          name="username"
+          placeholder="username"
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
