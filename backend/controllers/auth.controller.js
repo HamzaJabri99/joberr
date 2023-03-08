6dc38cddb53de4fd748eea623e24308add401ff7
unknown <jabri.oldd@gmail.com>
2023-03-08 23:12:01 +0100

    creating logout in backend

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -45,6 +45,13 @@ export const login = async (req, res, next) => {
 };
 export const logout = async (req, res) => {
   try {
+    res
+      .clearCookie("accessToken", {
+        sameSite: "none",
+        secure: true,
+      })
+      .status(200)
+      .send("Logged out!");
   } catch (error) {
     next(error);
   }
