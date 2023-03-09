09a4a52039057781cf0a85db5d54b1678b6990df
unknown <jabri.oldd@gmail.com>
2023-03-09 19:07:15 +0100

    designing register page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,55 +26,54 @@ function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-     element:<Layout/>,
-     children:[
-      {
-        path:'/',
-        element:<Home/>
-      },
-      {
-        path:'/gigs',
-        element:<Gigs/>
-      },
-      {
-        path:'/gig/:id',
-        element:<Gig/>
-      },
-      {
-        path:'/orders',
-        element:<Orders/>
-      },
-      {
-        path:'/mygigs',
-        element:<MyGigs/>
-      },
-      {
-        path:'/add',
-        element:<Add/>
-      },
-      {
-        path:'/messages',
-        element:<Messages/>
-      },
-      {
-        path:'/message/:id',
-        element:<Message/>
-      }
-     ]
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/gigs",
+          element: <Gigs />,
+        },
+        {
+          path: "/gig/:id",
+          element: <Gig />,
+        },
+        {
+          path: "/orders",
+          element: <Orders />,
+        },
+        {
+          path: "/mygigs",
+          element: <MyGigs />,
+        },
+        {
+          path: "/add",
+          element: <Add />,
+        },
+        {
+          path: "/messages",
+          element: <Messages />,
+        },
+        {
+          path: "/message/:id",
+          element: <Message />,
+        },
+        {
+          path: "/login",
+          element: <Login />,
+        },
+        {
+          path: "/register",
+          element: <Register />,
+        },
+      ],
     },
-   {
-    path:'/login',
-    element:<Login/>
-   },
-   {
-    path:'/register',
-    element:<Register/>
-   }
   ]);
   return (
     <div>
       <RouterProvider router={router} />
-
     </div>
   );
 }
--- frontend/src/pages/register/Register.jsx
+++ frontend/src/pages/register/Register.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
-
+import React from "react";
+import "./Register.scss";
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <div className="register">
+      <form>
+        <div className="left">
+          <h1>Create an account</h1>
+          <label htmlFor="">Username</label>
+          <input name="username" type="text" placeholder="joeDoe" />
+          <label htmlFor="">Email</label>
+          <input name="email" type="email" placeholder="email" />
+          <label htmlFor="">Password</label>
+          <input name="password" type="password" />
+          <label htmlFor="">Profile Picture</label>
+          <input type="file" />
+          <label htmlFor="">Country</label>
+          <input name="country" type="text" placeholder="Usa" />
+          <button type="submit">Register</button>
+        </div>
+        <div className="right">
+          <h1>I want to become a seller</h1>
+          <div className="toggle">
+            <label htmlFor="">Activate the seller account</label>
+            <label className="switch">
+              <input type="checkbox" />
+              <span className="slider round"></span>
+            </label>
+          </div>
+          <label htmlFor="">Phone Number</label>
+          <input name="phone" type="text" placeholder="+212 6 567 891 84" />
+          <label htmlFor="">Description</label>
+          <textarea
+            placeholder="A short description about you"
+            name="desc"
+            id=""
+            cols="30"
+            rows="10"
+          ></textarea>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default Register
+export default Register;
