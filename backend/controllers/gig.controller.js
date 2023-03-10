809b59f90756bbc9258100312528cca6d81b1032
unknown <jabri.oldd@gmail.com>
2023-03-10 18:46:43 +0100

    filtring gigs with mongo

--- backend/controllers/gig.controller.js
+++ backend/controllers/gig.controller.js
@@ -1,7 +1,31 @@
 import Gig from "../models/gig.model.js";
 import errorHandle from "../utils/errorHandle.js";
-export const getGigs = async (req, res, next) => {};
-export const getGig = (req, res, next) => {};
+export const getGigs = async (req, res, next) => {
+  const q = req.query;
+  const filters = {
+    ...(q.userId && { userId: q.userId }),
+    ...(q.cat && { cat: q.cat }),
+    ...((q.min || q.max) && {
+      price: { ...(q.min && { $gt: q.min }), ...(q.max && { $lt: q.max }) },
+    }),
+    ...(q.search && { title: { $regex: q.search, $options: "i" } }),
+  };
+  try {
+    const gigs = await Gig.find(filters);
+    return res.status(200).json(gigs);
+  } catch (error) {
+    next(error);
+  }
+};
+export const getGig = async (req, res, next) => {
+  try {
+    const gig = await Gig.findById(req.params.id);
+    if (!gig) return next(errorHandle(404, "There's no gig"));
+    return res.status(200).json(gig);
+  } catch (error) {
+    next(error);
+  }
+};
 export const createGig = async (req, res, next) => {
   if (!req.isSeller)
     return next(errorHandle(403, "You have to be a seller to create a gag!"));
