de820a0ca98e4e8c754cef5a7e58d290856226bd
unknown <jabri.oldd@gmail.com>
2023-03-10 16:25:26 +0100

    creating gig functionality

--- backend/controllers/gig.controller.js
+++ backend/controllers/gig.controller.js
@@ -1,3 +1,19 @@
-export const getGig=(req,res)=>{
-    res.send('hi')
-}
+import Gig from "../models/gig.model.js";
+import errorHandle from "../utils/errorHandle.js";
+export const getGigs = async (req, res, next) => {};
+export const getGig = (req, res, next) => {};
+export const createGig = async (req, res, next) => {
+  if (!req.isSeller)
+    return next(errorHandle(403, "You have to be a seller to create a gag!"));
+  const gig = new Gig({
+    userId: req.userId,
+    ...req.body,
+  });
+  try {
+    const savedGig = await gig.save();
+    return res.status(201).json(savedGig);
+  } catch (error) {
+    next(error);
+  }
+};
+export const deleteGig = (req, res, next) => {};
--- backend/routes/gig.route.js
+++ backend/routes/gig.route.js
@@ -1,5 +1,14 @@
-import express from "express"
-import { getGig } from "../controllers/gig.controller.js";
-const router =express.Router();
-router.get('/',getGig);
+import express from "express";
+import {
+  createGig,
+  deleteGig,
+  getGig,
+  getGigs,
+} from "../controllers/gig.controller.js";
+import { verifyToken } from "../middleware/jwt.js";
+const router = express.Router();
+router.post("/", verifyToken, createGig);
+router.get("/", verifyToken, getGigs);
+router.get("/:id", verifyToken, getGig);
+router.delete("/:id", verifyToken, deleteGig);
 export default router;
