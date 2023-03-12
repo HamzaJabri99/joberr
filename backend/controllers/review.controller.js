d2d3b819fc4070fdd721a81a3967414b272eb512
unknown <jabri.oldd@gmail.com>
2023-03-12 20:37:50 +0100

    deleting review functionality

--- backend/controllers/review.controller.js
+++ backend/controllers/review.controller.js
@@ -12,20 +12,39 @@ export const getReviews = async (req, res, next) => {
 };
 export const createReview = async (req, res, next) => {
   const gig = await Gig.findById(req.body.gigId);
-  try {
-    if (req.userId === gig.userId)
-      return next(errorHandle(403, "You can't review Your Gig"));
+  if (req.userId === gig.userId)
+    return next(errorHandle(403, "You can't review Your Gig"));
 
-    const review = new Review({
+  const review = new Review({
+    userId: req.userId,
+    gigId: req.body.gigId,
+    desc: req.body.desc,
+    star: req.body.star,
+  });
+  try {
+    const rev = await Review.findOne({
       userId: req.userId,
       gigId: req.body.gigId,
-      desc: req.body.desc,
-      star: req.body.star,
     });
-    await review.save();
-    res.status(200).send("review posted!");
+    if (rev) return next(errorHandle(403, "You can't Review More than Once"));
+    const savedReview = await review.save();
+    Gig.findOneAndUpdate(req.body.gigId, {
+      $inc: { totalStars: req.body.star, starNumber: 1 },
+    });
+    res.status(201).send(savedReview);
   } catch (error) {
     next(error);
   }
 };
-export const deleteReview = async (req, res, next) => {};
+export const deleteReview = async (req, res, next) => {
+  const review = await Review.findById(req.params.id);
+  try {
+    if (!review) return next(errorHandle(404, "no review"));
+    if (req.userId !== review.userId)
+      return next(errorHandle(403, "not authorized"));
+    const rev = await Review.findByIdAndDelete(req.params.id);
+    return res.status(201).send(rev);
+  } catch (error) {
+    next(error);
+  }
+};
