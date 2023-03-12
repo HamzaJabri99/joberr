fb96dac3d38a896a93b648be7b17e40dcc8d3708
unknown <jabri.oldd@gmail.com>
2023-03-13 00:23:03 +0100

    creating review section to add reviews

--- frontend/src/components/review/Review.jsx
+++ frontend/src/components/review/Review.jsx
@@ -1,40 +1,43 @@
+import { useQuery } from "@tanstack/react-query";
+import request from "../../utils/request";
 import "./Review.scss";
-const Review = () => {
+const Review = ({ review }) => {
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["userReview"],
+    queryFn: () =>
+      request.get(`/users/${review.userId}`).then((res) => res.data),
+  });
+  console.log(data);
   return (
     <div className="review">
-      <div className="user">
-        <img
-          className="pp"
-          src="https://images.pexels.com/photos/839586/pexels-photo-839586.jpeg?auto=compress&cs=tinysrgb&w=1600"
-          alt=""
-        />
-        <div className="info">
-          <span>Garner David</span>
-          <div className="country">
-            <img
-              src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1fa-1f1f8.png"
-              alt=""
-            />
-            <span>United States</span>
+      {isLoading ? (
+        "loading..."
+      ) : error ? (
+        "error"
+      ) : (
+        <div className="user">
+          <img className="pp" src={data.img || "/imgs/noavatar.jpg"} alt="" />
+          <div className="info">
+            <span>{data.username}</span>
+            <div className="country">
+              <img
+                src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1fa-1f1f8.png"
+                alt=""
+              />
+              <span>{data.country}</span>
+            </div>
           </div>
         </div>
-      </div>
+      )}
       <div className="stars">
-        <img src="/imgs/star.png" alt="" />
-        <img src="/imgs/star.png" alt="" />
-        <img src="/imgs/star.png" alt="" />
-        <img src="/imgs/star.png" alt="" />
-        <img src="/imgs/star.png" alt="" />
-        <span>5</span>
+        {Array(review.star)
+          .fill()
+          .map((img, i) => (
+            <img key={i} src="/imgs/star.png" alt="" />
+          ))}
+        <span>{review.star}</span>
       </div>
-      <p>
-        I just want to say that art_with_ai was the first, and after this, the
-        only artist Ill be using on Fiverr. Communication was amazing, each and
-        every day he sent me images that I was free to request changes to. They
-        listened, understood, and delivered above and beyond my expectations. I
-        absolutely recommend this gig, and know already that Ill be using it
-        again very very soon
-      </p>
+      <p>{review.desc}</p>
       <div className="helpful">
         <span>Helpful?</span>
         <img src="/imgs/like.png" alt="" />
--- frontend/src/components/reviews/Reviews.jsx
+++ frontend/src/components/reviews/Reviews.jsx
@@ -1,14 +1,28 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import React from "react";
 import request from "../../utils/request";
 import Review from "../review/Review";
-import "./reviews.scss";
+import "./Reviews.scss";
 const Reviews = ({ gigId }) => {
+  const queryClient = useQueryClient();
   const { isLoading, error, data } = useQuery({
     queryKey: ["reviews"],
     queryFn: () => request.get(`/reviews/${gigId}`).then((res) => res.data),
   });
-  console.log(data);
+  const mutation = useMutation({
+    mutationFn: (newReview) => {
+      return request.post("/reviews", newReview);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["reviews"]);
+    },
+  });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const desc = e.target[0].value;
+    const star = e.target[1].value;
+    mutation.mutate({ gigId, desc, star });
+  };
   return (
     <div className="reviews">
       <h2>Reviews</h2>
@@ -19,6 +33,20 @@ const Reviews = ({ gigId }) => {
         ? "error"
         : data?.map((review) => <Review key={review._id} review={review} />)}
       <hr />
+      <div className="addReview">
+        <h3>Add a review</h3>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="desc" placeholder="write your review" />
+          <select name="" id="" placeholder="Stars">
+            <option value={1}>1</option>
+            <option value={2}>2</option>
+            <option value={3}>3</option>
+            <option value={4}>4</option>
+            <option value={5}>5</option>
+          </select>
+          <button>Send</button>
+        </form>
+      </div>
     </div>
   );
 };
