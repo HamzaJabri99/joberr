cc5590669af59ff47f167d3aebe838ac80216879
unknown <jabri.oldd@gmail.com>
2023-03-13 17:21:27 +0100

    create and fetch orders on backend

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -1,3 +1,36 @@
-export const getOrder=(req,res)=>{
-res.send('order works')
-}
+import Gig from "../models/gig.model.js";
+import Order from "../models/order.model.js";
+import errorHandle from "../utils/errorHandle.js";
+
+export const createOrder = async (req, res, next) => {
+  try {
+    const gig = await Gig.findById(req.params.gigId);
+    const order = new Order({
+      gigId: gig._id,
+      img: gig.cover,
+      title: gig.title,
+      buyerId: req.userId,
+      sellerId: gig.userId,
+      price: gig.price,
+      payment_intent: "Temporary",
+    });
+    if (gig.userId === req.userId)
+      return next(errorHandle(403, "You can't order to yourself"));
+    await order.save();
+    res.status(200).send("successful order");
+  } catch (error) {
+    next(error);
+  }
+};
+export const getOrders = async (req, res, next) => {
+  try {
+    const orders = await Order.find({
+      ...(req.isSeller ? { sellerId: req.userId } : { buyerId: req.userId }),
+      isCompleted: true,
+    });
+    if (!orders) return next(errorHandle(404, "there are no orders!"));
+    return res.status(200).json(orders);
+  } catch (error) {
+    next(error);
+  }
+};
--- backend/routes/order.route.js
+++ backend/routes/order.route.js
@@ -1,5 +1,7 @@
 import express from "express";
-import { getOrder } from "../controllers/order.controller.js";
-const route=express.Router();
-route.get('/',getOrder)
-export default route
+import { getOrders, createOrder } from "../controllers/order.controller.js";
+import { verifyToken } from "../middleware/jwt.js";
+const route = express.Router();
+route.get("/", verifyToken, getOrders);
+route.post("/:gigId", verifyToken, createOrder);
+export default route;
