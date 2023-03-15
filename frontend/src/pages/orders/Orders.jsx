5eff3ce3dad78e256159b9248ee06c1242510772
unknown <jabri.oldd@gmail.com>
2023-03-15 19:27:45 +0100

    creating conversations from orders page

--- frontend/src/pages/orders/Orders.jsx
+++ frontend/src/pages/orders/Orders.jsx
@@ -2,13 +2,30 @@ import "./Orders.scss";
 import { useQuery } from "@tanstack/react-query";
 import request from "../../utils/request";
 import UserOrder from "../../components/userOrderTd/UserOrderTd";
+import { useNavigate } from "react-router-dom";
 const Orders = () => {
   const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+  const location = useNavigate();
   const { isLoading, error, data } = useQuery({
     queryKey: ["orders"],
     queryFn: () => request.get("/orders").then((res) => res.data),
   });
-
+  const handleContact = async (order) => {
+    const sellerId = order.sellerId;
+    const buyerId = order.buyerId;
+    const id = sellerId + buyerId;
+    try {
+      const res = await request.get(`/conversations/${id}`);
+      location(`/message/${res.data.id}`);
+    } catch (error) {
+      if (error.response.status === 404) {
+        const res = await request.post("/conversations/", {
+          to: currentUser.isSeller ? buyerId : sellerId,
+        });
+        location(`/message/${res.data.id}`);
+      }
+    }
+  };
   return (
     <div className="orders">
       {isLoading ? (
@@ -42,7 +59,12 @@ const Orders = () => {
                     id={currentUser.isSeller ? order.buyerId : order.sellerId}
                   />
                   <td>
-                    <img src="/imgs/message.png" className="message" alt="" />
+                    <img
+                      src="/imgs/message.png"
+                      className="message"
+                      alt=""
+                      onClick={() => handleContact(order)}
+                    />
                   </td>
                 </tr>
               ))}
